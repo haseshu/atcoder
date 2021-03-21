2757f9c9517e87d5115b314cf666d72944985609
haseshu <haseshu@gmail.com>
2021-03-21 21:54:43 +0900

    ABC085C - Otoshidama

--- atcoder/AtCoderBeginnersSelection/ABC085C-Otoshidama.py
+++ atcoder/AtCoderBeginnersSelection/ABC085C-Otoshidama.py
@@ -1,17 +1,17 @@
 ### きっと2文探索とか使う気がする
 N, Y = map(int, input().split())
 clear = False
+man, gosen, sen =  -1,-1,-1
 for i in range (N+1):
     if clear is True or i * 10000 > Y:
         break
     for j in range(N + 1 - i):
         if clear is True or i * 10000 + j * 5000> Y:
             break
-        for k in range(N + 1 - i - j):
-            if clear is True or i * 10000 + j * 5000 + 1000 * k> Y:
-                break
-            if (clear is False) and (10000 * i + 5000 * j + 1000 * k == Y) :
+        if (clear is False) and (10000 * i + 5000 * j + 1000 * (N-i-j) == Y) :
                 clear = True
-                print("{} {} {}".format(i,j,k))
-if clear is False:
-    print("{} {} {}".format(-1,-1,-1))
+                man =i
+                gosen=j
+                sen=N-i-j
+                break
+print("{} {} {}".format(man,gosen,sen))
