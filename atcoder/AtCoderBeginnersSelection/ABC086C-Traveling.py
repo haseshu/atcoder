86b3dbc3eb064e3d240dd343fa8b6ad764b00d96
haseshu <haseshu@gmail.com>
2021-03-23 23:19:04 +0900

    WA ABC086C - Traveling

--- atcoder/AtCoderBeginnersSelection/ABC086C-Traveling.py
+++ atcoder/AtCoderBeginnersSelection/ABC086C-Traveling.py
@@ -2,32 +2,28 @@ N = int(input())
 T=[]
 X=[]
 Y=[]
+T.append(0)
+X.append(0)
+Y.append(0)
 for i in range(N):
     a,b,c=list(map(int,input().split()))
     T.append(a)
     X.append(b)
     Y.append(c)
 
-t, x, y =0, 0, 0
+#print("{},{},{}".format(T,X,Y))
 have_route =True
-for i in range(len(T)):
+for i in range(1,N+1):
 #    for j in range(T[i]):
 #    print("{}={},{},{}".format(i,t,x,y))
-    subt = T[i] - t
-    subx = X[i] - x
-    suby = Y[i] - y
+    subt = T[i] - T[i-1]
+    subx = X[i] - X[i-1]
+    suby = Y[i] - Y[i-1]
 #    print("{},{},{}".format(T[i],X[i],Y[i]))
     length = abs(subx) + abs(suby)
-    if (subt - length) % 2 ==0 and subt >= length:
-        #T[i]の地点にはいける。
-#        print("{} is OK".format(i))
-        t = T[i]
-        x = X[i]
-        y = Y[i]
-    else:
-        have_route=False
-        break
-if have_route:
+    if (subt % 2 != length % 2) or (subt < length):
+        have_route = False
+if have_route is True:
     print("YES")
 else:
     print("NO")
