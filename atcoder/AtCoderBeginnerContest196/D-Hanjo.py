6be2200aeb5809091505dbf84a17cff557f5ce50
haseshu <haseshu@gmail.com>
2021-03-28 21:02:31 +0900

    clear ABC196 D. Hanjo

--- atcoder/AtCoderBeginnerContest196/D-Hanjo.py
+++ atcoder/AtCoderBeginnerContest196/D-Hanjo.py
@@ -5,7 +5,8 @@ H, W, A, B = map(int, input().split())
 ans = 0
 
 def dfs(i, j, tatami, A, B):
-    print("({},{},{},{})".format(i,j,A,B))
+#    print("({},{},{},{})".format(i,j,A,B))
+#    print(tatami)
     kekka = True
     for k in range(H):
         #print("{},{}".format(k,tatami[k]))
@@ -15,41 +16,48 @@ def dfs(i, j, tatami, A, B):
     if kekka == True:
         global ans
         ans += 1
-        print(tatami)
-        print("True")
+#        print(tatami)
+#        print("True")
         return 1
     if j >= W:
-        print("end")
+#        print("end")
         return 0
     if i >= H:
-        t = tatami[:][:]
+        t = [row[:] for row in tatami]
         dfs(0, j+1, t, A, B)
     
     #print("({},{},{},{})".format(i,j,A,B))
     if j < W and i < H:
         if tatami[i][j] !=0:
-            t = tatami[:][:]
-            dfs(0, j+1, t, A, B)
+            t = [row[:] for row in tatami]
+            dfs(i+1, j, t, A, B)
         else:
             for k in range(3):
+                #print("k={}".format(k))
+                #print("({},{},{},{})".format(i,j,A,B))
                 if k == 0 and B != 0:
-                    tatami[i][j] = 1
-                    t = tatami[:][:]
+                    t = [row[:] for row in tatami]
+                    t[i][j] = 1
+                    #print("{} = tatami".format(id(tatami)))
+                    #print("{} = t".format(id(t)))
                     dfs(i+1, j, t, A, B -1)
                 elif k ==1 and A != 0:
                     if i+1 < H and tatami[i+1][j] == 0:
-                        tatami[i][j] = 2
-                        tatami[i+1][j] = 2
-                        t = tatami[:][:]
+                        t = [row[:] for row in tatami]
+                        t[i][j] = 2
+                        t[i+1][j] = 2
+                        
                         dfs(i+2, j, t, A - 1, B)
                 elif k == 2 and A != 0:
+                    #print("({},{},{},{})".format(i,j,A,B))
                     if j+1 < W and tatami[i][j+1] == 0:
-                        tatami[i][j] = 3
-                        tatami[i][j+1] = 3
-                        t = tatami[:][:]
+                        t = [row[:] for row in tatami]
+                        t[i][j] = 3
+                        t[i][j+1] = 3
                         dfs(i+1, j, t, A - 1, B)
 
-tatami_map=[[0 for i in range(H)] for j in range(W)]
+tatami_map=[[0 for i in range(W)] for j in range(H)]
+#tatami_map=[0 for i in range(H)] * W
 #print(tatami_map)
 dfs(0,0, tatami_map, A, B)
 print(ans)
