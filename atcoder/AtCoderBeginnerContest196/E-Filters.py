612da5591fe06a38be8b9492245b891e87550f32
haseshu <haseshu@gmail.com>
2021-03-30 22:32:49 +0900

    playing ABC196 E. Filters

--- atcoder/AtCoderBeginnerContest196/E-Filters.py
+++ atcoder/AtCoderBeginnerContest196/E-Filters.py
@@ -18,6 +18,33 @@ x = list(map(int, input().split()))
 f=[-999] * Q
 
 ##ここを再帰ではなく、1回式を作ったらそれを保存して使いまわしにすればタイムアウトは無くなるのでは。
+
+def saiki(q, n):
+    if n == 0:
+        if t[n] == 1:
+            return a[n] + x[q]
+        if t[n] == 2:
+            return max([a[n], x[q]])
+        if t[n] == 3:
+            return min([a[n], x[q]])
+    elif n == N-1:
+        for i in range(q):
+            if t[n] == 1:
+                print(saiki(i, n-1) + a[n])
+            elif t[n] == 2:
+                print(max([a[n],saiki(i, n-1)]))
+            elif t[n] == 3:
+                print(min([a[n],saiki(i, n-1)]))
+    else:
+        if t[n] == 1:
+            return saiki(q, n-1) + a[n]
+        elif t[n] == 2:
+            return max([a[n],saiki(q, n-1)])
+        elif t[n] == 3:
+            return min([a[n],saiki(q, n-1)])
+
+saiki(Q,N-1)
+"""
 def saiki(i, n):
     if n == 0:
         if t[n] == 1:
@@ -36,3 +63,4 @@ def saiki(i, n):
 
 for i in range(Q):
     print(saiki(i, N-1))
+"""
